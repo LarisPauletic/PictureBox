474b410ea5cc132021f3ef3bccecc60567007ee4
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-01-25 13:50:08 +0100

    Add files via upload

---
+++ PictureBox/PictureBox/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace PictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.picBx1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBx1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(260, 347);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(270, 58);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Prikaži sliku";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // picBx1
+            // 
+            this.picBx1.Image = global::PictureBox.Properties.Resources.dsadasdadsad;
+            this.picBx1.Location = new System.Drawing.Point(125, 39);
+            this.picBx1.Name = "picBx1";
+            this.picBx1.Size = new System.Drawing.Size(531, 257);
+            this.picBx1.TabIndex = 0;
+            this.picBx1.TabStop = false;
+            this.picBx1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.picBx1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBx1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBx1;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
